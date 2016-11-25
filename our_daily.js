a0ca66325dc36cf03365c8849d7745c2d0c5ab76
emeznar <ericmeznarich@gmail.com>
2016-11-25 10:04:53 -0600

    add ignore file with keys

---
+++ our_daily.js
@@ -0,0 +1,290 @@
+window.onload = function() {
+
+    //get current time as page loads - before DCT is called
+    DisplayCurrentTime();
+
+    //get drawing board on page load
+    getDrawingBoard();
+
+    //Get current date
+    function DisplayCurrentTime() {
+        var date = new Date();
+        //make a list of days then assign the corresponding number to variable
+        var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday",
+            "Thursday", "Friday", "Saturday"
+        ];
+        var day = dayNames[date.getDay()];
+
+        //list months then assign corresponding number to variable
+        var monthNames = ["", "January", "February", "March", "April",
+            "May", "June", "July", "August", "September", "October", "November", "December"
+        ];
+        var month = monthNames[date.getMonth()];
+
+        //Get day of month as a number
+        var dayofmonth = date.getDate();
+        //Get time - hours - separate am/pm then minutes//add leading zeros
+        function addZeros(value) {
+            if (value < 10) {
+                return "0" + value;
+            } else {
+                return value;
+            }
+        }
+        var hours = addZeros(date.getHours()) > 12 ? date.getHours() - 12 : date.getHours();
+        var am_pm = date.getHours() >= 12 ? "PM" : "AM";
+        var minutes = addZeros(date.getMinutes());
+
+        //Add greeting
+        var greeting = '';
+        if (date.getHours() < 12) {
+            greeting = "Good morning, this is  ";
+        } else if (date.getHours() > 15) {
+            greeting = "Good evening, this is  ";
+        } else {
+            greeting = "Good afternoon, this is  ";
+        }
+        //assign all info obtained to variable and send to main page
+        var time = greeting + day + ", " + month + " " + dayofmonth + ".  The time is " +
+            hours.toString() + ":" + minutes + " " + am_pm;
+        //console.log(time);
+        document.querySelector("#displaydateTime").innerHTML = time;
+
+        //update time every 60 seconds from page load
+        var updateTimeOnPage;
+
+        function updateTime() {
+            updateTimeOnPage = setInterval(DisplayCurrentTime, 1000 * 60);
+        }
+        updateTime();
+    }
+};
+//Query api for weather information on top of page
+function getWeatherdata() {
+
+    var weather_key = config.MY_WEATHERKEY;
+    var lattitude = 38.8114;
+    var longitude = -89.9532;
+    var params = weather_key + "/" + lattitude + "," + longitude;
+    var todaysConditions = "";
+    var todaysLow = "";
+    var todaysHigh = "";
+
+    $.ajax({
+        dataType: 'jsonp', //workaround for CORS error from local server
+        url: "https://api.darksky.net/forecast/" + params,
+        type: 'GET',
+
+        success: function(response) {
+            console.log(response);
+            //Today's weather
+            var currentTemp = Math.round(response.currently.temperature);
+            var currentWeather = response.currently.summary;
+            //get icon variable from response and run through function for src assignment
+            var todaysweathericon = response.daily.data[0].icon;
+            document.getElementById('todayicon').src = getWeatherIcon(todaysweathericon);
+            //get forecast summary from response
+            var todaysConditions = (response.daily.data[0].summary); //.substring(0,13);
+            //get low and high from response
+            var todaysLow = Math.round(response.daily.data[0].temperatureMin);
+            var todaysHigh = Math.round(response.daily.data[0].temperatureMax);
+            //Tomorrow's weather - do same as above for response data
+            var day1weathericon = response.daily.data[1].icon;
+            document.getElementById('day1icon').src = getWeatherIcon(day1weathericon);
+            var day1Conditions = (response.daily.data[1].summary); //.substring(0,13);
+            var day1Low = Math.round(response.daily.data[1].temperatureMin);
+            var day1High = Math.round(response.daily.data[1].temperatureMax);
+            //Next day's weather
+            //Get day again and add 2 for display - nextday
+            var todaydate = new Date();
+            //shift start of list by 2 to get day 2 days from now
+            var dayNames = ["Tuesday", "Wednesday", "Thursday", "Friday",
+                "Saturday", "Sunday", "Monday"
+            ];
+            var nextday = dayNames[todaydate.getDay()];
+            var day2weathericon = response.daily.data[2].icon;
+            document.getElementById('day2icon').src = getWeatherIcon(day2weathericon);
+            var day2Conditions = (response.daily.data[2].summary); //.substring(0,13);
+            var day2Low = Math.round(response.daily.data[2].temperatureMin);
+            var day2High = Math.round(response.daily.data[2].temperatureMax);
+            //Send the rest of the info to page
+            //Current conditions
+            $("#displaycurrent").text("Current weather conditions are " + currentWeather +
+                " and " + currentTemp + String.fromCharCode(176));
+            //Todays forecast
+            $("#todaysforecast").text(todaysConditions);
+            $("#todayHiLow").text(todaysLow + String.fromCharCode(176) + "|" + todaysHigh + String.fromCharCode(176));
+            //Tomorrow's forecast
+            $("#day1forecast").text(day1Conditions);
+            $("#tomorrowHiLow").text(day1Low + String.fromCharCode(176) + "|" + day1High + String.fromCharCode(176));
+            //next day's forecast (enter day with variable)
+            $("#day2day").text(nextday);
+            $("#day2forecast").text(day2Conditions);
+            $("#nextDayHiLow").text(day2Low + String.fromCharCode(176) + "|" + day2High + String.fromCharCode(176));
+        },
+        error: function(response) {
+            console.log('Error in Operation');
+        }
+    });
+}
+getWeatherdata();
+
+function getWeatherIcon(x) {
+    //if response/day/icon = defined text, assign image
+    var weathericon = "";
+    if (x === "partly-cloudy-day") {
+        weathericon = "img/partlycloudyday.png";
+    } else if (x === "partly-cloudy-night") {
+        weathericon = "img/partlycloudynight.png";
+    } else if (x === "cloudy") {
+        weathericon = "img/cloudy.png";
+    } else if (x === "fog") {
+        weathericon = "img/fog.png";
+    } else if (x === "wind") {
+        weathericon = "img/wind.png";
+    } else if (x === "sleet") {
+        weathericon = "img/sleet.png";
+    } else if (x === "snow") {
+        weathericon = "img/snow.png";
+    } else if (x === "rain") {
+        weathericon = "img/rain.png";
+    } else if (x === "clear-night") {
+        weathericon = "img/clearnight.png";
+    } else {
+        weathericon = "img/clearday.png";
+    }
+    return weathericon;
+}
+//Add school lunch menu pdf to iframe
+//get current month abbreviation a different way from above and use for menu link
+function getSchoolLunchMenu() {
+    var today = new Date();
+    today2 = today.toString();
+    var monthAbrev = (today2.substr(4, 4)).trim();
+    var lunchMenuLink = "http://www.ecusd7.org/departments/food_service/menus/2016-17/" +
+        monthAbrev + "/ElemLunch.pdf#toolbar=0"; //toolbar=0 disables save/print option at top
+    document.getElementById('lunch-menu').src = lunchMenuLink;
+}
+getSchoolLunchMenu();
+
+//get calendar link from git ignore file
+function getCalendar(){
+    var calendarLink = config.MY_EMAILLINK;
+        document.getElementById('calendar-frame').src = calendarLink;
+}
+getCalendar();
+//create the shopping list module in angular
+var myShoppingListApp = angular.module('myShoppingListApp', []);
+//give focus to add item textbox on load
+$("#textbox").focus();
+//use scope in function to bind list data between js and html file - empty list to start
+myShoppingListApp.controller('firstController', function($scope) {
+    $scope.todo_lists = [];
+    $scope.new_item = "";
+    $scope.form_error = "";
+
+    $scope.addItem = function() {
+        if ($scope.new_item.length === 0) {
+            $scope.form_error = true;
+        } else {
+            $scope.form_error = false;
+            //add item to list
+            $scope.todo_lists.push($scope.new_item);
+            $scope.new_item = '';
+            //Give focus to add new item textbox after submission
+            $("#textbox").focus();
+        }
+    };
+
+    //delete all items from the list
+    $scope.deleteItem = function(i) {
+        if (confirm("OK to delete ALL items?")) {
+            $scope.todo_lists = [];
+        }
+    };
+});
+//capitalize first letter of word
+myShoppingListApp.filter('capitalize', function() {
+    return function(input) {
+        return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+    };
+});
+
+//Display drawing board for notes/pictures/etc...
+function getDrawingBoard() {
+    var myCanvas = document.getElementById("myCanvas");
+    //create color variable to provide hard coded option to change color of drawing
+    var curColor = "#806600";
+    //Listen for click events on drawing board - change color appropriately
+    // when the draw button is clicked
+    $("#draw-button").click(function() {
+        // change color
+        curColor = "#806600";
+    });
+    $("#erase-button").click(function() {
+        // change brush to background color for erase
+        curColor = "#FFFFF0";
+    });
+
+    if (myCanvas) {
+        var isDown = false;
+        var ctx = myCanvas.getContext("2d");
+        var canvasX, canvasY;
+        ctx.lineWidth = 5;
+
+        $(myCanvas)
+            .mousedown(function(e) {
+                isDown = true;
+                ctx.beginPath();
+                canvasX = e.pageX - myCanvas.offsetLeft;
+                canvasY = e.pageY - myCanvas.offsetTop;
+                ctx.moveTo(canvasX, canvasY);
+            })
+            .mousemove(function(e) {
+                if (isDown !== false) {
+                    canvasX = e.pageX - myCanvas.offsetLeft;
+                    canvasY = e.pageY - myCanvas.offsetTop;
+                    ctx.lineTo(canvasX, canvasY);
+                    ctx.strokeStyle = curColor;
+                    ctx.stroke();
+                }
+            })
+            .mouseup(function(e) {
+                isDown = false;
+                ctx.closePath();
+            });
+    }
+}
+
+
+
+//function to clear canvas when clear button clicked
+function clearCanvas() {
+    // refresh drawing board
+    myCanvas.width = myCanvas.width;
+    getDrawingBoard();
+}
+
+//update elements on page at defined intervals
+var updateWeather; //update weather every hour
+var updatelunchMenu; //update menu every day
+function updatePage() {
+    updateWeather = setInterval(getWeatherdata, 1000 * 60 * 60);
+    updatelunchMenu = setInterval(getSchoolLunchMenu, 1000 * 60 * 60 * 24);
+}
+updatePage();
+//TODO: add reload button for calendar
+//TODO: Create variables for api keys and dummy google calendar link before uploading to github
+//TODO: Better calendar/plugin?/ or direct link to iframe with google in agenda mode
+//TODO: Create variables for api keys and dummy google calendar link before uploading to github
+//TODO: Double check view on smaller screens, laptop, ipad, etc...
+//DONE: add weather icons in place of text at top of page
+//DONE: Add refresh button to erase entire drawing board
+//DONE: Refresh the lunch menu every day
+//DONE: refresh time every minute and weather every hour
+//DONE: Break out lunch menu from original function so it doesn't refresh every minute
+//DONE: Add erase and draw buttons to change color from blue to white
+//DONE: re-render time so it is updated periodically
+//DONE: Add pdf picker (change link to pdf based on date - i.e. 9 = Oct in link)
+//DONE:Add scroll bar to shopping list - go off page instead of moving everythihng down
+//DONE: Build shopping list with angular
